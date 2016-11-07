fb2c535b6405f7208e94f9495e9770ee67911e85
sharon van sprang <sharon_van_sprang@hotmail.com>
2016-11-07 16:52:20 +0100

    add achievement model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107154658) do
+ActiveRecord::Schema.define(version: 20161107155104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "achievements", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_achievements_on_user_id", using: :btree
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.text     "bio"
@@ -41,5 +48,6 @@ ActiveRecord::Schema.define(version: 20161107154658) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "achievements", "users"
   add_foreign_key "profiles", "users"
 end
