1a93a2f95c2ce38e38d179a89f5d133258696b2e
Catharina van Leeuwen <catharina.m.vanleeuwen@gmail.com>
2016-11-08 16:04:18 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :life_goals, only: [:index, :show] do
+    resources :challenges, only: [:index, :show]
+  end
+
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
