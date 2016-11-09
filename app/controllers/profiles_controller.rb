0ceebe4de20135dad73584b5e9bcc201cb1fb366
Paulien Sewuster <paulien.sewuster@gmail.com>
2016-11-09 14:25:53 +0100

    Add ability to edit profile and access account settings

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,44 +2,68 @@ class ProfilesController < ApplicationController
 
 # before_action :find_user, only: [:new]
 
-  def new
-    @profile = Profile.new
-  end
-
-
-
+def new
+  @profile = Profile.new
+end
 
 def create
-    @profile = Profile.new(profile_params)
-    @profile.user = current_user
+  @profile = Profile.new(profile_params)
+  @profile.user = current_user
 
-    if @profile.save
-      redirect_to root_path
-    else
-      render :new
-    end
-
+  if @profile.save
+    redirect_to user_profile_path(current_user, current_user.profile)
+  else
+    render :new
   end
+end
 
-
-
-  def show
+def show
 # raise 'paulien'
+end
+
+def edit
+  @profile = current_user.profile
+end
+
+def update
+  @profile = current_user.profile
+  @profile.update(profile_params)
+
+  if @profile.save
+    redirect_to user_profile_path(current_user, current_user.profile)
+  else
+    render :new
   end
+end
 
 
-
-  private
-
-
-
-  def profile_params
-    params.require(:profile).permit(:bio)
+def destroy
+  @user = current_user
+  if @user.destroy
+    redirect_to root_url, notice: "User deleted."
+  else
+    render_error_message
   end
-
-  def find_user
-    @user = User.find(params[:user_id])
-  end
+end
 
 
+
+
+
+
+
+
+
+
+
+
+private
+
+def profile_params
+  params.require(:profile).permit(:bio)
+end
+
+# def find_user
+#   @user = User.find(params[:user_id])
+# end
 end
