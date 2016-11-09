95cf868015d9940074a423f4f781a97b19ea9de4
Paulien Sewuster <paulien.sewuster@gmail.com>
2016-11-09 12:39:28 +0100

    Add new profile form, show profile, in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users,
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :users, except: [:index, :create, :new, :show, :update, :edit, :destroy] do
+     resources :profiles
+
+    end
+
 
   resources :life_goals, only: [:index, :show] do
     resources :challenges, only: [:index, :show]
   end
+
+
 
   root to: 'pages#home'
 
