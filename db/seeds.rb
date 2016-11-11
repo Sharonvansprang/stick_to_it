a5e7187d8deb04bc77a4dc2b6107960c35254b1d
Catharina van Leeuwen <catharina.m.vanleeuwen@gmail.com>
2016-11-11 17:33:11 +0100

    TGOD in the house

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Achievement.destroy_all
 Challenge.destroy_all
 LifeGoal.destroy_all
 
