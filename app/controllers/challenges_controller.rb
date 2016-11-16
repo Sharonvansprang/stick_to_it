802d45a1d34ef224200d1cf2db2241e72413fadd
Paulien Sewuster <paulien.sewuster@gmail.com>
2016-11-16 14:55:23 +0100

    Add functionality to select a buddy

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,6 +3,9 @@ class ChallengesController < ApplicationController
 
   def show
     @challenge = Challenge.find(params[:id])
+    @achievements = Achievement.where(challenge: @challenge) - Achievement.where(user: current_user)
+
+
   end
 
 end
