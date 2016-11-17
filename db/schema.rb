7ee1444e2d7e463c76273238305cce94607c6020
Catharina van Leeuwen <catharina.m.vanleeuwen@gmail.com>
2016-11-17 15:42:43 +0100

    add blogMVC +fakerseed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114152650) do
+ActiveRecord::Schema.define(version: 20161117105402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20161114152650) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
+  end
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "intro"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "challenges", force: :cascade do |t|
